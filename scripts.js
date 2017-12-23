2e81eb317e7251f5137d74465a6b96fbfb1e8668
theRealPadster <padspambox@gmail.com>
2017-12-22 21:30:05 -0500

    Add forecast call to grab today's forecast, Split utilities off into separate js file, Display forecast for next 24h

--- scripts.js
+++ scripts.js
@@ -12,12 +12,13 @@ function getLocation() {
 
 function showPosition(position) {
 
+    //call for current weather
     $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=8e62e050c29fc061b770beda0908a51c",
+        url: "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=metric&appid=8e62e050c29fc061b770beda0908a51c",
         success: function(data) {
 
             var city = data.name;
-            var temp = Math.round(data.main.temp - 273);
+            var temp = Math.round(data.main.temp);
             var weather = data.weather[0].main;
 
             $("#city").text(city);
@@ -26,7 +27,49 @@ function showPosition(position) {
 
             setBackground(temp);
         }
-    })
+    });
+
+    //call for forecast
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=metric&appid=8e62e050c29fc061b770beda0908a51c",
+        success: function(data) {
+            var now = new Date();
+            //TODO - can I make "tomorrow" one line?
+            var tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+
+            //TODO - this filters out so it shows a 24 hour period, tweak it. only show till end of day, or what?
+            data.list.some(function (item) {
+                var itemDate = getDate(item);
+                if (itemDate.getTime() < tomorrow.getTime()) {
+                    console.log(itemDate);
+                    var elem = makeWeatherChunkElement(item, itemDate);
+                    $("#forecastSection").append(elem);
+                }
+                else {
+                    return true;
+                }
+            });
+        }
+    });
+}
+
+function makeWeatherChunkElement(weatherItem, date) {
+
+    var options = { month:'short', day: 'numeric', hour: 'numeric', minute:'numeric' };
+
+    var forecastChunk = "<div class='forecastChunk'></div>";
+    // var time = "<p class='time'>" + date.getHours() + ":" + date.getMinutes() + "</p>";
+    var time = "<p class='time'>" + date.toLocaleString("en-US", options) + "</p>";
+    var temp = "<p class='temp'>" + Math.round(weatherItem.main.temp) + "</p>";
+    var weather = "<p class='weather'>" + weatherItem.weather[0].main + "</p>";
+
+    var toAdd = $(forecastChunk)
+    toAdd.append(time);
+    toAdd.append(temp);
+    toAdd.append(weather);
+
+    return toAdd;
 }
 
 //TODO - validate, or parsley?
@@ -41,29 +84,6 @@ function submitPreferences() {
     setCookie("preferenceValue", preferenceValue, 7);
 
     setCookie("preferencesSet", true, 7);
-}
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
 }
 
 function checkPreferencesSet() {
---
+++ utilities.js
@@ -0,0 +1,37 @@
+function getDate(item) {
+    var unix_timestamp = item.dt;
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(unix_timestamp*1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    return date;
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
